4c598a9bd0f9035b313e8961d91f6888b5c00a04
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2021-07-17 22:58:08 +0100

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+//require express
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+//get index route
+app.get('/', (req, res) =>{
+//   res.sendFile(__dirname + '/index.html');
+    res.send("live ...")
+})
+
+
+
+//app listener
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+})
