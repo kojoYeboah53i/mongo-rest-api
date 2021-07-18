212fa9d4618bcf301c1d613d706d0872915df6b9
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2021-07-18 18:09:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //require mongoose
-const mongoose = require('mongoose');
+const { MongoClient }  = require('mongodb');
 
 
 //get index route
@@ -14,7 +14,17 @@ app.get('/', (req, res) =>{
 })
 
 //connect to mongodb
-mongoose.connect('mongodb://localhost:27017/test');
+// mongoose.connect('mongodb://localhost:27017/test');
+const uri = "mongodb+srv://kojoyeboah53i:<password>@cluster0.2fm1y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true } =>
+  {
+    console.log('Connected successfully to database');
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 //app listener
