8174f8cfa78a06333e1f0d1f52022bdb52d533b8
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2021-07-18 20:37:03 +0100

    update

--- routes/posts.js
+++ routes/posts.js
@@ -8,6 +8,16 @@ router.get('/', (req, res) =>{
  //   res.sendFile(__dirname + '/index.html');
         res.send("live ...")
  })
+//post route
+router.post('/', (req, res) =>{
+    console.log(req.body);
+    // const title = req.body.title;
+    // const content = req.body.content;
+    // const userId = req.user._id;
+    // posts.create(title, content, userId)
+    //     .then(post => res.json(post))
+    //     .catch(err => res.status(500).send(err));
+});
 
 router.get('/theposts', (req, res) => {
     res.send("posts ...")
