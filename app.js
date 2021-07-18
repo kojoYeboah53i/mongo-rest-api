4448d3884177c19fa830c9ac24288b599654561c
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2021-07-18 18:01:40 +0100

    update

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//require mongoose
+const mongoose = require('mongoose');
+
 
 //get index route
 app.get('/', (req, res) =>{
@@ -10,9 +13,13 @@ app.get('/', (req, res) =>{
     res.send("live ...")
 })
 
+//connect to mongodb
+mongoose.connect('mongodb://localhost:27017/test');
 
 
 //app listener
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 })
+
+
--- readme.md
+++ readme.md
@@ -3,3 +3,8 @@
      mongo_db : "mongoose",
      dotenv : "dotenv",
  ]
+
+
+ database cred
+ user:           kojoyeboah53i
+ password:       saints24_1738
